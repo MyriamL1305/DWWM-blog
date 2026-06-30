7cada4f6a3ccdfad36cc9ecc7affcdcf8821f3c6
Myriam LERIDER <minimir1@yahoo.fr>
2026-06-30 10:37:23 -0400

    feat:  Article routes

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 
 //Routes Categories
 Route::get('/categories', [CategoryController::class, 'index']);
+
+Route::get('/articles',[ArticleController::class, 'index']);
