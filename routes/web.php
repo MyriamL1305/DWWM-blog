8eead669fd78318d64ca1fb6b27cd62813d7a792
Myriam LERIDER <minimir1@yahoo.fr>
2026-07-01 09:35:10 -0400

    feat: add Category routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\CategoryController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Routes Categories
+Route::get('/categories', [CategoryController::class, 'index']);
