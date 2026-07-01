72977bd520e57b6c783535dc66de4c8cd36ba0b2
Myriam LERIDER <minimir1@yahoo.fr>
2026-07-01 09:35:10 -0400

    feat: add Category routes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
     return view('home');
 });
 
+// Routes Articles
 Route::get('/articles', [ArticleController::class, 'index']);
+
+//Routes Categories
+Route::get('/categories', [CategoryController::class, 'index']);
