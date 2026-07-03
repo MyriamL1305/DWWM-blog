5fee66c14b42cd4011b5d1054a51ccdc06abff9f
Myriam LERIDER <minimir1@yahoo.fr>
2026-07-03 09:06:14 -0400

    fix: remove duplicate article route definition

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,10 @@ Route::get('/', function () {
     return view('home');
 });
 
-<<<<<<< HEAD
+
 
 //Routes Categories
 Route::get('/categories', [CategoryController::class, 'index']);
 
 Route::get('/articles',[ArticleController::class, 'index']);
-=======
-Route::get('/articles', [ArticleController::class, 'index']);
->>>>>>> f0cb64b (feat: display articles list)
+
