42d1ea09698e6b5213fdd6b08abac1000ea4cfa1
Myriam LERIDER <minimir1@yahoo.fr>
2026-07-24 08:11:07 -0400

    suppression de l'authentification des users

--- routes/web.php
+++ routes/web.php
@@ -36,16 +36,3 @@ Route::prefix('admin')->name('admin.')->group(function () {
 });
 
 
-// Routes générées par Breeze
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy'); 
-    }); 
-
-    
-require __DIR__.'/auth.php';
